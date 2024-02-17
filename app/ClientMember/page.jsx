de52635bf1c40f36ebe1a7c3b8babcf0fc9ab5a1
The-renaissance-man <mutumabrianm@gmail.com>
2024-02-17 22:10:57 +0300

    protecting pages

--- app/ClientMember/page.jsx
+++ app/ClientMember/page.jsx
@@ -1,10 +1,19 @@
 "use client"
+import {useSession} from "next-auth/react"
+import { redirect } from "next/navigation"
 import React from 'react'
 
+
 const Member = () => {
+ const {data:session} = useSession({
+  required: true,
+  onUnauthenticated(){
+    redirect("/api/auth/signin?callbackUrl=/ClientMember")
+  }
+ })
   return (
     <div>
-      <h2>This is an addition</h2> <h1>Member Client Session</h1>
+      <h1>Member Client Session</h1>
       <p>{session?.user?.email}</p>
       <p>{session?.user?.role}</p>
     </div>
